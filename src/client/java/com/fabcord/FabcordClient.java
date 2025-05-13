01618fdd25bba9b8e24727ecf40e6fb4cf48b21e
a29 <alexkirwan29@gmail.com>
2025-05-13 19:01:07 +0800

    import of fabric mod template from https://fabricmc.net/develop/template/

---
+++ src/client/java/com/fabcord/FabcordClient.java
@@ -0,0 +1,10 @@
+package com.fabcord;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FabcordClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
